6c080273250685d78eb5978d638887617a6b3811
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-02-18 16:38:46 +0200

    updates

---
+++ listSumAvg/list.js
@@ -0,0 +1,18 @@
+function sumOfElements(input) {
+  let totalSum = 0;
+  for (let i = 0; i < input.length; i++) {
+    totalSum = totalSum+ input[i];
+  }
+
+  return totalSum;
+}
+let result=sumOfElements([1,2,2]);
+console.log(result);
+
+function averageOfArray(b-'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''){
+   return sumOfElements(list)/list.length;
+   
+   }
+let listAverage=averageOfArray([1,2,3]);
+console.log(listAverage);
+
